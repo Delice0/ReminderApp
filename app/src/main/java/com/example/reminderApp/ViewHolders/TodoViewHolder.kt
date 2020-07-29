43e94a978c47ed52e2a7617abfbec2fbefafb3c4
Delice <59118341+Delice0@users.noreply.github.com>
2020-07-29 02:12:15 +0200

    Both changes to ui and DAO
    - Query now also orders by priority
    - Removed deprecated classes
    - TodoViewHolder -> ui changes
    - Added round border xml
    - Changed from complex layouts to óne simple constraintlayout for Todo-list

--- app/src/main/java/com/example/reminderApp/ViewHolders/TodoViewHolder.kt
+++ app/src/main/java/com/example/reminderApp/ViewHolders/TodoViewHolder.kt
@@ -1,7 +1,6 @@
 package com.example.reminderApp.ViewHolders
 
-import android.graphics.Color
-import android.graphics.drawable.Drawable
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.CheckBox
 import android.widget.TextView
@@ -9,35 +8,53 @@ import com.example.reminderApp.Listeners.TodoItemListener
 import com.example.reminderApp.Models.Todo
 import com.example.reminderApp.R
 import com.example.reminderApp.Utils.DateUtil
-import org.xmlpull.v1.XmlPullParser
 
 class TodoViewHolder(view: View) : BaseViewHolder<Todo, TodoItemListener?>(view) {
     private var title: TextView? = null
     private var description: TextView? = null
     private var priority: TextView? = null
     private var created: TextView? = null
-    private var duedate: TextView? = null
+    private var duedate_date: TextView? = null
+    private var duedate_time: TextView? = null
     private var checkbox: CheckBox? = null
+
+    val splitDateAndTimeRegex = "(\\d+\\.\\w{3})\\s(\\d+:\\d+)".toRegex()
 
     init {
         title = itemView.findViewById(R.id.todo_custom_title)
         description = itemView.findViewById(R.id.todo_custom_description)
         priority = itemView.findViewById(R.id.todo_custom_priority)
         created = itemView.findViewById(R.id.todo_custom_created)
-        duedate = itemView.findViewById(R.id.todo_custom_duedate)
+        duedate_date = itemView.findViewById(R.id.todo_custom_duedate_date)
+        duedate_time = itemView.findViewById(R.id.todo_custom_duedate_time)
         checkbox = itemView.findViewById(R.id.todo_custom_checkbox)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun bind(item: Todo, listener: TodoItemListener?) {
+        val i = item.dueDate.format(DateUtil.dateTimeFormat_simple)
+
+        var onlyDate = ""
+        var onlyTime = ""
+
+        if (splitDateAndTimeRegex.matches(i)) {
+            onlyDate = splitDateAndTimeRegex.matchEntire(i)!!.groupValues[1]
+            onlyTime = splitDateAndTimeRegex.matchEntire(i)!!.groupValues[2]
+        }
+
+        if (item.description.length >= 25) {
+            description?.text = item.description.substring(0,22) + "..."
+        } else {
+            description?.text = item.description
+        }
+
+        duedate_date?.text = onlyDate
+        duedate_time?.text = onlyTime
+
         title?.text = item.title
-        description?.text = item.description
         priority?.text = item.priority.toString()
-
-        // Simplify datetime in UI
-        created?.text = item.createdDate.format(DateUtil.dateTimeFormat_simple)
-        duedate?.text = item.dueDate.format(DateUtil.dateTimeFormat_simple)
-
         checkbox?.isChecked = item.isDone
+        created?.text = item.createdDate.format(DateUtil.dateTimeFormat_simple)
 
         itemView.setOnClickListener { listener?.onClickItem(itemView, adapterPosition) }
         checkbox?.setOnClickListener { listener?.onClickItem(checkbox!!, adapterPosition) }
