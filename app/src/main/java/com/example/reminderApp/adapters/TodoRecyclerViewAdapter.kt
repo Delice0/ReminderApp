a616bc8ddbd7df8e748cfde7dd5bca2bfbea9004
Delice <ibrahimdelice96@gmail.com>
2020-11-15 21:13:43 +0100

    Adapter setFilter and getItem methods implemented

--- app/src/main/java/com/example/reminderApp/adapters/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/reminderApp/adapters/TodoRecyclerViewAdapter.kt
@@ -3,11 +3,11 @@ package com.example.reminderApp.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.reminderApp.R
 import com.example.reminderApp.listeners.TodoItemListener
 import com.example.reminderApp.models.Todo
-import com.example.reminderApp.R
-import com.example.reminderApp.viewHolders.TodoViewHolder
 import com.example.reminderApp.viewHolders.BaseViewHolder
+import com.example.reminderApp.viewHolders.TodoViewHolder
 
 class TodoRecyclerViewAdapter : RecyclerView.Adapter<BaseViewHolder<*, TodoItemListener?>>() {
     private var todos: List<Todo> = emptyList()
@@ -39,4 +39,20 @@ class TodoRecyclerViewAdapter : RecyclerView.Adapter<BaseViewHolder<*, TodoItemL
         this.listener = listener
     }
 
+    fun setFilter(queryList: List<Todo>) {
+        if (!todos.isNullOrEmpty()) {
+            todos = arrayListOf()
+        }
+
+        (todos as ArrayList<Todo>).addAll(queryList)
+        notifyDataSetChanged()
+    }
+
+    fun getItem(position: Int): Todo {
+        try {
+            return todos[position]
+        } catch (e: RuntimeException) {
+            throw RuntimeException("Item does not exist!")
+        }
+    }
 }
